ae96d78237f4354b31494d0e014bf5a587e7ce16
Bavard Rahdar <bavardrahdar@gmail.com>
2015-05-04 10:30:59 -0400

    Fixed non user pins page redirect

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,17 +1,21 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   respond_to :html
-def index
+
+  def index
     @pins = Pin.all
+    respond_with(@pins)
   end
 
   def show
+    respond_with(@pin)
   end
 
   def new
     @pin = current_user.pins.build
+    respond_with(@pin)
   end
 
   def edit
@@ -19,42 +23,29 @@ def index
 
   def create
     @pin = current_user.pins.build(pin_params)
-    if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
-    else
-      render action: 'new'
-    end
+    @pin.save
+    respond_with(@pin)
   end
 
   def update
-    if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
-    else
-      render action: 'edit'
-    end
+    @pin.update(pin_params)
+    respond_with(@pin)
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url
+    respond_with(@pin)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find_by(id: params[:id])
+      @pin = Pin.find(params[:id])
     end
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+      redirect_to pins_path, notice:"Not authorized to edit this pin" if @pin.nil?
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description, :image)
-    end
-
 
 
     def pin_params
